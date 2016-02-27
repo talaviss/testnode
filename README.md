79d51c89966d40312aee282191ab3d73016213b6
talaviss <talaviss@gmail.com>
2016-02-27 22:03:29 +0200

    main app.js added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Webstorm 11
-integrating with the webstorm ide 
+Full inegration with the the 11 webstorm ide
+
+ ## Phases to be done
+ 1. Running initial setup
+ 2. Defining globals
+
--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 /**
- * Created by tal on 27/02/2016.
+ * Created by tal avissar on 27/05/2014.
+ *
+ * this is the main enterance file
  */
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1337;
+
+http.createServer((req, res) = > {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Hello World\n');
+}).
+listen(port, hostname, () = > {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
+;
