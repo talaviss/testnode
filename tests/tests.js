690f60aa3177083848c1a770f71b1902b5af8473
talaviss <talaviss@gmail.com>
2016-02-27 23:20:16 +0200

    added tests with mocha

---
+++ tests/tests.js
@@ -0,0 +1,32 @@
+/**
+ * Created by tal on 27/02/2016.
+ * description: some redicolus tests
+ */
+
+
+var should = require('should');
+var assert = require('assert');
+
+var threeSeconds = require('seconds');
+
+
+describe('Test framework', function(){
+    it('should have mocha installed and running', function(){
+        assert.equal(true,true);
+    })
+    it('should have the should library installed and running for fluent testing',function()
+    {
+        true.should.eql(true);
+    })
+
+})
+
+describe('Array', function() {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1,2,3].indexOf(5));
+            assert.equal(-1, [1,2,3].indexOf(0));
+        });
+    });
+});
+
